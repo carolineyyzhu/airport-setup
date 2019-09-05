16c1e4de121ad741f03c81cf26f072cb963b2285
Blake Aschenbrener <blakeasch@yahoo.com>
2019-09-05 19:03:49 -0400

    Airport Class

---
+++ src/Airport.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+/**
+ * @author blakeaschenbrener
+ *
+ */
+public final class Airport implements Comparable<Airport> {
+
+	@Override
+	public int compareTo(Airport o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
