b454e0258e49188a545941059ea043d7686c3120
Blake Aschenbrener <blakeasch@yahoo.com>
2019-09-06 09:05:52 -0400

    Updated Airport Class

--- src/airtravel/Airport.java
+++ src/airtravel/Airport.java
@@ -3,21 +3,71 @@ package airtravel; /**
  */
 
 /**
- * @author blakeaschenbrener
+ * This class creates airport objects with codes and min connection times.
  *
  */
 public final class Airport implements Comparable<Airport> {
 	
-	private Airport(
-			String code,
-			Duration connectionTimeMin) {
+	
+	private final String code;
+	private final Duration connectionTimeMin;
+	
+	/**
+	 * Constructor
+	 * @param code: A string representation of the airport code
+	 * @param connectionTimeMin: the min connection time of a flight
+	 */
+	private Airport(String code,Duration connectionTimeMin) {
+		this.code = code;
+		this.connectionTimeMin = connectionTimeMin;
 		
 	}
+	
+	/**
+	 * Public constructor: Catches any null values
+	 * @param code: A string representation of the airport code
+	 * @param connectionTimeMin: the min connection time of a flight
+	 * @return the created airport
+	 */
+	public static final Airport of(String code, Duration connectionTimeMin) {
+		//Check for null inputs
+		if (code == null | connectionTimeMin == null) {
+			throw null;
+		}
+		//Create Instance of Airpoirt
+		Airport retVal = new Airport(code, connectionTimeMin);
+		return retVal;
+	}
+	
+	//Getter for Code
+	public final String getCode() {
+		return code;
+	}
+	
+	//Getter for connectionTimeMin
+	public final Duration getConnectionTimeMin() {
+		return connectionTimeMin;
+	}
+	
+	//Override Equals method based on code
+	public final boolean equals(Airport airport) {
+		return this.code.equals(airport.code);
+	}
+	
+	//Override hash code based on code
+	public final int hashCode() {
+		return this.code.hashCode();
+	}
+	
+	//Override to string based on code
+	public final String toString() {
+		return this.code.toString();
+	}
 
+	//Override compare to based on code
 	@Override
-	public int compareTo(Airport o) {
-		// TODO Auto-generated method stub
-		return 0;
+	public final int compareTo(Airport airport) {
+		return this.code.compareTo(airport.code);
 	}
 
 }
