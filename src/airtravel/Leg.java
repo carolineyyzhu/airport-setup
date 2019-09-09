07371aa169791f7c5bcb9024cdb027bd789d5309
Caroline Zhu <carolinezhu@Carolines-MacBook-Air.local>
2019-09-09 11:48:23 -0400

    Leg added

---
+++ src/airtravel/Leg.java
@@ -0,0 +1,17 @@
+package airtravel;
+
+public final class Leg {
+
+    private Airport origin;
+    private Airport destination;
+
+    private Leg(Airport origin, Airport destination) {
+        origin = this.origin;
+        destination = this.destination;
+    }
+
+    public static final Leg of() {
+
+    }
+
+}
