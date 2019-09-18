3f6568b0ca308a93f96c49b9db6cf71acb5d4567
Blake Aschenbrener <blakeasch@yahoo.com>
2019-09-18 17:33:50 -0400

    Kinda sorta better. fix reserve

--- src/airtravel/FlightPolicy.java
+++ src/airtravel/FlightPolicy.java
@@ -15,35 +15,38 @@ public final class FlightPolicy extends AbstractFlight {
 	}
 	
 	public static final FlightPolicy of(Flight flight, BiFunction<SeatConfiguration, FareClass, SeatConfiguration> policy) {
-		//Create Instance of Airport
-		if() {
-
-		}
 		return new FlightPolicy(flight, policy);
 	}
 	
-	public static final Flight strict(Flight policyFlight) {
+	public static final Flight strict(Flight flight) {
 		//thres some more stuff to get the enum map to cooperate but you got the bulk of it holy shit
 		//dab dab I have no idea what im doing help me fix big brain
-		BiFunction<SeatConfiguration, FareClass, SeatConfiguration> policy = (a,b) -> policyFlight.hasSeats(b) ? policyFlight.seatsAvailable(b) : a;
-		return FlightPolicy.of(policyFlight, policy);
+		BiFunction<SeatConfiguration, FareClass, SeatConfiguration> policy = (a,b) -> flight.hasSeats(b) ? flight.seatsAvailable(b) : a;
+		return FlightPolicy.of(flight, policy).flight;
 	}
 	
 	public static final Flight restrictedDuration(Flight flight, Duration durationMax) {
 		if (flight.isShort(durationMax)){
-			//do strict
+			//returns a strict policy on short flights,
+			BiFunction<SeatConfiguration, FareClass, SeatConfiguration> policy = (a,b) -> flight.hasSeats(b) ? flight.seatsAvailable(b) : a;
+			return FlightPolicy.of(flight, policy).flight;
 		} else {
-			//return the same seat configuration as on the underlying flight
+			//returns the same seat configuration as on the underlying flight
+			BiFunction<SeatConfiguration, FareClass, SeatConfiguration> policy = (a,b) -> a;
+			return FlightPolicy.of(flight, policy).flight;
+			
 		}
 		
 		
 	}
 	
-	public static final Flight reserve(Flight policyFlight, int reserve) {
+	public static final Flight reserve(Flight flight, int reserve) {
+		BiFunction<SeatConfiguration, FareClass, SeatConfiguration> policy = (a,b) -> (flight.hasSeats(b)) ? (flight.seatsAvailable(b) - reserve) : a;
+		return FlightPolicy.of(flight, policy).flight;
 		
 	}
 	
-	public static final Flight limited(Flight policyFlight) {
+	public static final Flight limited(Flight flight) {
 		
 	}
 
