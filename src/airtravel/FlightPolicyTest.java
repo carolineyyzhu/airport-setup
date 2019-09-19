3112c4e44056b06b5e840a72b655677d0c9696dc
Blake Aschenbrener <blakeasch@yahoo.com>
2019-09-19 16:22:30 -0400

    Smol flight policy update

--- src/airtravel/FlightPolicyTest.java
+++ src/airtravel/FlightPolicyTest.java
@@ -44,7 +44,7 @@ public class FlightPolicyTest {
      * Test to apply the limited policy to a flight
      */
     @Test
-    public void limitedPolicyFlight() {
+    public void limitedReservePolicyFlight() {
         Airport airportA = Airport.of("AA12", Duration.ofMinutes(30));
         Airport airportB = Airport.of("BB8", Duration.ofMinutes(30));
         Leg leg = Leg.of(airportA, airportB);
@@ -72,6 +72,7 @@ public class FlightPolicyTest {
         }
         assertTrue(seatsAccurate);
     }
+
 }
 
 //    /**
