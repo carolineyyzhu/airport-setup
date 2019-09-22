bb416e77cc6280b2d4e4e3ea5ea00cc7b0677d0f
Caroline Zhu <carolinezhu@Carolines-MacBook-Air.local>
2019-09-22 12:01:19 -0400

    RouteNode updated

--- src/airtravel/RouteNode.java
+++ src/airtravel/RouteNode.java
@@ -1,6 +1,7 @@
 package airtravel;
 
 import java.util.Objects;
+import java.util.Set;
 
 public final class RouteNode implements Comparable<RouteNode>{
     private final Airport airport;
@@ -16,24 +17,33 @@ public final class RouteNode implements Comparable<RouteNode>{
     public static final RouteNode of(Airport airport, RouteTime arrivalTime, RouteNode previous) {
         Objects.requireNonNull(airport,"Airport input cannot be null");
         Objects.requireNonNull(arrivalTime,"Arrival time cannot be null");
-
         return new RouteNode(airport, arrivalTime, previous);
     }
 
     public static final RouteNode of(Flight flight, RouteNode previous) {
         Objects.requireNonNull(flight,"Flight input cannot be null");
-
-        return new RouteNode(flight.destination(), new RouteTime(flight.arrivalTime()), previous);
+        return RouteNode.of(flight.destination(), new RouteTime(flight.arrivalTime()), previous);
     }
 
     public static final RouteNode of (Airport airport) {
         Objects.requireNonNull(airport,"Airport input cannot be null");
-
-        return new RouteNode(airport, RouteTime.UNKNOWN(), null);
+        return RouteNode.of(airport, RouteTime.UNKNOWN(), null);
     }
 
     public final Boolean isArrivalTimeKnown() {
         return !this.getArrivalTime().equals(RouteTime.UNKNOWN());
+    }
+
+    public final RouteTime departureTime() {
+        if(!isArrivalTimeKnown())
+            throw new NullPointerException("Route time cannot be unknown");
+        return this.getArrivalTime().plus(this.getAirport().getConnectionTimeMin());
+    }
+
+    public Set<Flight> availableFlights(FareClass fareClass) {
+        if(!isArrivalTimeKnown())
+            throw new NullPointerException("Route time cannot be unknown");
+        return this.getAirport().availableFlights(this.departureTime().getTime(), fareClass);
     }
 
     public Airport getAirport() {
@@ -51,7 +61,6 @@ public final class RouteNode implements Comparable<RouteNode>{
     @Override
     public int compareTo(RouteNode o) {
         Objects.requireNonNull(airport,"Route node input cannot be null");
-
         return o.getArrivalTime().compareTo(this.getArrivalTime());
     }
 }
