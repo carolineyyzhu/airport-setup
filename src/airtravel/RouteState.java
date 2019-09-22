8a0c322f24b11a939786a9d9b0aaf3581c32284e
Blake Aschenbrener <blakeasch@yahoo.com>
2019-09-22 12:28:42 -0400

    Update RouteState.java

--- src/airtravel/RouteState.java
+++ src/airtravel/RouteState.java
@@ -2,22 +2,53 @@ package airtravel;
 
 import java.time.LocalTime;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.NavigableSet;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 import java.util.Set;
+import java.util.TreeSet;
 
 final class RouteState {
 	
-	private Map<Airport, RouteNode> airportNode;
-	private final NavigableSet<RouteNode> unreached;
+	private Map<Airport, RouteNode> airportNode = new HashMap<>();
+	private final NavigableSet<RouteNode> unreached = new TreeSet<>();
 	
 	private RouteState(Set<Airport> airports,Airport origin, LocalTime departureTime) {
-		this.airportNode = Collections.EMPTY_MAP;
+        Objects.requireNonNull(airports,"Set<Airport> input cannot be null");
+        Objects.requireNonNull(origin,"Arrival time cannot be null");
+        Objects.requireNonNull(departureTime,"LocalTime input cannot be null");
+		
 		RouteNode originNode = RouteNode.of(origin, new RouteTime(departureTime), null);
 		this.airportNode.put(origin, originNode);
-		airports.stream().forEach(n -> RouteNode.of(n, new RouteTime(), null) );
+		airports.stream().forEach(n -> this.airportNode.put(n, RouteNode.of(n, RouteTime.UNKNOWN(), null)));
+		airports.stream().forEach(n -> this.unreached.add(RouteNode.of(n, RouteTime.UNKNOWN(), null)));
 		
-		this.unreached = 
+	}
+	
+	void replaceNode(RouteNode routeNode) {
+		airportNode.put(routeNode.getAirport(), routeNode);
+		
+	}
+	
+	boolean allReached() {
+		return unreached.isEmpty();
+		
+	}
+	
+	RouteNode closestUnreached() {
+		if (!allReached()) {
+			return Collections.min(unreached);
+		}else{
+			throw new NoSuchElementException ("All nodes have been reached");
+		}
+		
+	}
+	
+	RouteNode airportNode(Airport airport) {
+		return airportNode.get(airport);
+		
 	}
 
 }
