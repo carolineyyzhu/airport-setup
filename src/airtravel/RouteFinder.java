2bfe19298e40d85c10de2df607ba3c3e0a5e8366
Blake Aschenbrener <blakeasch@yahoo.com>
2019-09-25 21:24:19 -0400

    Beep Boop

--- src/airtravel/RouteFinder.java
+++ src/airtravel/RouteFinder.java
@@ -57,7 +57,7 @@ public final class RouteFinder {
 				Duration length = Duration.between(currentFlight.departureTime(), currentFlight.arrivalTime());
 				RouteTime arrivalTime = new RouteTime(currentFlight.arrivalTime());
 				
-				if (arrivalTime.compareTo(arrivalTime.plus(length)) < 0) {
+				if (arrivalTime.compareTo(arrivalTime.plus(length)) < 0) { //TODO: this if statement is likely wrong
 					RouteNode newNode = RouteNode.of(currentFlight, currentNode);
 					currentRoutes.replaceNode(newNode);	
 				}
