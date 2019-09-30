338bbbefec10801a948356b373fa328812f483f3
Blake Aschenbrener <blakeasch@yahoo.com>
2019-09-30 18:26:46 -0400

    Fixed

--- src/airtravel/RouteTime.java
+++ src/airtravel/RouteTime.java
@@ -11,7 +11,6 @@ import java.util.Objects;
 public final class RouteTime implements Comparable<RouteTime>{
 	
 	private final LocalTime routeTime;
-	public  static  final  RouteTime  UNKNOWN = null;
 	
 	/**
 	 * This constructor creates a new RouteTime given a localTime
@@ -22,6 +21,10 @@ public final class RouteTime implements Comparable<RouteTime>{
 		this.routeTime = routeTime;
 	}
 	
+	//creates a routeTime where the time is unknown and null
+	public static final RouteTime UNKNOWN(){
+		return new RouteTime(null);
+	}
 	
 	//checks to see if the route time is known
 	public boolean isKnown() {
@@ -44,7 +47,7 @@ public final class RouteTime implements Comparable<RouteTime>{
 		if (isKnown()) {
 			return new RouteTime(routeTime.plus(duration));
 		} else {
-			return UNKNOWN;
+			return UNKNOWN();
 		}
 		
 	}
